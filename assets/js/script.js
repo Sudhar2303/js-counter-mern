3f9663efc7f6928c52ba958586141335d7a9e00d
Sudhar2303 <sudharsananuma23@gmail>
2024-02-27 15:30:59 +0530

    counter function added

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const incrementFunction = () => {
+    var element = document.getElementById("counter-number");
+    element.textContent++;
+}
+
+const decrementFunction = () => {
+    var element = document.getElementById("counter-number");
+    element.textContent--;
+}
